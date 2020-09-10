28e23c7b28ee2071fbd635140ea54c5b55539f13
Surammo <surammo@gmail.com>
2020-09-11 00:05:08 +0530

    Updates

---
+++ precache-manifest.771170924df646e83f49168eb5fd8acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b048d0ec6ed58662b11074d3b659ded",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7e4778cda644bd622db5",
+    "url": "/monsters-rolodex/static/css/main.fda3d481.chunk.css"
+  },
+  {
+    "revision": "482d3b0974c298f0d8e6",
+    "url": "/monsters-rolodex/static/js/2.a998f851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a998f851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4778cda644bd622db5",
+    "url": "/monsters-rolodex/static/js/main.c1ed5bdd.chunk.js"
+  },
+  {
+    "revision": "a66be9ef1b64aa4a17f9",
+    "url": "/monsters-rolodex/static/js/runtime-main.b3323ec8.js"
+  }
+]);
